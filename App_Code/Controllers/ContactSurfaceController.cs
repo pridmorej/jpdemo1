8813c291c5e50f9ea1ba039a56959f517953ea96
pridmorej <gitgub@pridmorej.freeserve.co.uk>
2015-02-19 07:44:42 +0000

    Finished adding Contact Me page

---
+++ App_Code/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,36 @@
+﻿using jpdemo1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using umbraco;
+
+namespace jpdemo1.Controllers
+{
+    /// <summary>
+    /// Summary description for ContactSurfaceController
+    /// </summary>
+    public class ContactSurfaceController : Umbraco.Web.Mvc.SurfaceController
+    {
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("<p>Name: {0}</p>", model.Name);
+                sb.AppendFormat("<p>Email: {0}</p>", model.Email);
+                sb.AppendFormat("<p>Phone: {0}</p>", model.Phone);
+                sb.AppendFormat("<p>{0}</p>", model.Message);
+
+                library.SendMail("noreply@cms.jeremypridmore.com", "jeremy.pridmore@googlemail.com", "CMS Contact Form", sb.ToString(), true);
+
+                return RedirectToUmbracoPage(model.ThankYouPage);
+            }
+
+            return CurrentUmbracoPage();
+        }
+    }
+}
---
+++ App_Code/Models/ContactModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace jpdemo1.Models
+{
+    /// <summary>
+    /// Summary description for ContactModel
+    /// </summary>
+    public class ContactModel
+    {
+        private int thankYouPage;
+
+        public ContactModel()
+        {
+        }
+
+        public ContactModel(int ThankYouPage = 0)
+        {
+            // TODO: Complete member initialization
+            this.thankYouPage = ThankYouPage;
+        }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        [DisplayName("Enter a Message")]
+        public string Message { get; set; }
+        [Required]
+        public int ThankYouPage
+        {
+            get
+            {
+                return this.thankYouPage;
+            }
+            set
+            {
+                this.thankYouPage = value;
+            }
+        }
+    }
+}
